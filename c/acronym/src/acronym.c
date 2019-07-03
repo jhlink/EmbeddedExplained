9e66ce70a95752610f3e11312630b6d9f851ddb0
jhlink <jhlink@users.noreply.github.com>
2019-07-03 13:04:25 -0400

    feat: Handle basic_abbreviation for abbreviate

--- c/acronym/src/acronym.c
+++ c/acronym/src/acronym.c
@@ -15,7 +15,23 @@ char* abbreviate(const char *phrase) {
   char* result = NULL; 
 
   if ( phrase ) {
+    // Two is added for the starting letter and null termination 
+    size_t acronym_size = count_delims(phrase, " ") + 2;
+    size_t phrase_len = strlen(phrase);
 
+    result = (char*) malloc(sizeof(char) * acronym_size); 
+
+    assert(result != NULL);
+
+    int acronym_index = 0; 
+    result[acronym_index++] = phrase[0];
+    result[acronym_size - 1] = '\0';
+
+    for ( size_t i = 0; i < phrase_len - 1; i++ ) {
+      if (phrase[i] == ' ') {
+        result[acronym_index++] = phrase[i + 1]; 
+      }
+    }
   }
 
   return result;
