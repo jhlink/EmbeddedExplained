bb20c619b41cf12ca645b5bce00c937c0c156844
jhlink <jhlink@users.noreply.github.com>
2019-07-07 16:32:10 -0400

    panagram: Handle perfect lowercase 26-char input

--- c/pangram/src/pangram.c
+++ c/pangram/src/pangram.c
@@ -1,10 +1,31 @@
 #include "pangram.h"
 
+static bool is_alph_array_nonzero(int alpha[]) 
+{
+  bool result = true;
+  for ( int i = 0; i < ALPHABET_LENGTH; i++ ) {
+    result &= alpha[i];  
+  }
+  return result;
+}
+
 bool is_pangram(const char *sentence) {
   bool result = false;
-  if ( sentence ) {
 
+  if ( sentence ) {
+    int sentence_len = strlen(sentence);
+    if ( sentence_len >= ALPHABET_LENGTH ) {
+      int *alpha = (int*) calloc(ALPHABET_LENGTH, sizeof(int));
+      
+      for (int i = 0; i < sentence_len; i++) {
+        int alph_index = (sentence[i] - 97) % ALPHABET_LENGTH;
+        alpha[alph_index]++;
+      }
+
+      result = is_alph_array_nonzero(alpha);
+    }
   }
 
   return result;
 }
+
--- c/pangram/src/pangram.h
+++ c/pangram/src/pangram.h
@@ -1,7 +1,11 @@
 #ifndef PANGRAM_H
 #define PANGRAM_H
 
+#define ALPHABET_LENGTH 26
+
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 bool is_pangram(const char *sentence);
 
