d085c456a961806ce88cdeb3a951402575df2c90
jhlink <jhlink@users.noreply.github.com>
2019-07-09 16:55:14 -0400

    space_age: Resolve initial compilation errors

---
+++ c/space-age/src/space_age.h
@@ -0,0 +1,15 @@
+#ifndef SPACE_AGE_H
+#define SPACE_AGE_H
+
+#define EARTH 31557600
+#define MERCURY 0.2408467
+#define VENUS 0.61519726
+#define MARS 1.8808158
+#define JUPITER 11.862615
+#define SATURN 29.447498
+#define URANUS 84.016846
+#define NEPTUNE 164.79132
+
+float convert_planet_age(float value, unsigned long age);
+
+#endif
