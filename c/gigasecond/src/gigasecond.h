8b279a81f0a1e33f06d7cd2ede18161f91095de2
jhlink <jhlink@users.noreply.github.com>
2019-07-09 16:32:15 -0400

    gigasecond: Resolve compilation errors
    Exercise did not provide boilerplate files for error-free compilation.

---
+++ c/gigasecond/src/gigasecond.h
@@ -0,0 +1,11 @@
+#ifndef GIGASEC_H
+#define GIGASEC_H
+
+#include <time.h>
+
+time_t gigasecond_after(time_t raw_date);
+
+//time_t construct_date(
+//   time_t actual = gigasecond_after(construct_date(2015, 1, 24, 22, 0, 0));
+
+#endif
