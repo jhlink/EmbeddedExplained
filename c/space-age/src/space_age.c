4d2398d9491f84d7ea09e6ddc18a4c82b345c2e1
jhlink <jhlink@users.noreply.github.com>
2019-07-10 13:47:46 -0400

    space_age: Refactor function logic
    Implementation is unnecessarily complex.
    
    Normalize input to simplify logic.

--- c/space-age/src/space_age.c
+++ c/space-age/src/space_age.c
@@ -1,16 +1,6 @@
 #include "space_age.h"
 
-static bool float_equality(float a, float b, float tolerance) {
-  return fabs(a - b) < tolerance;
-}
-
 float convert_planet_age(float planet_time, unsigned long age) {
-  float result = 0;
-  if ( float_equality( planet_time, EARTH, .01 ) ) {
-    result = age / planet_time; 
-  } else { 
-    float planet_yr_sec_ratio = EARTH * planet_time;
-    result = age / planet_yr_sec_ratio;
-  }
-  return result;
+  float earth_yr_in_sec = SECONDS_PER_DAY * DAYS_ON_EARTH;
+  return age / ( planet_time * earth_yr_in_sec );
 }
