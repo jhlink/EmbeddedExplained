d8cdcd5ab5df7c2200fabd9bbe95294c396987dc
jhlink <jhlink@users.noreply.github.com>
2019-07-10 13:23:11 -0400

    space_age: Handle non-earth time ratios
    The EARTH constant is measured in seconds whereas the Mercury time
    ratio is measured in Earth years.
    
    To preserve accuracy,the non-earth planet ratio values are retained as
    floating point types in earth years to be converted to seconds when
    needed.

--- c/space-age/src/space_age.h
+++ c/space-age/src/space_age.h
@@ -1,7 +1,13 @@
 #ifndef SPACE_AGE_H
 #define SPACE_AGE_H
 
+#include <stdbool.h>
+#include <math.h>
+
+//  Equivalent to 365.25 Earth Days
 #define EARTH 31557600
+
+//  Subsequent planet values are in Earth Years
 #define MERCURY 0.2408467
 #define VENUS 0.61519726
 #define MARS 1.8808158
@@ -10,6 +16,6 @@
 #define URANUS 84.016846
 #define NEPTUNE 164.79132
 
-float convert_planet_age(float value, unsigned long age);
+float convert_planet_age(float planet_time, unsigned long age);
 
 #endif
