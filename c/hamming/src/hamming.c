2743fd9dba106f84aa748868b9876b6a7cdc14da
jhlink <jhlink@users.noreply.github.com>
2019-07-12 15:11:33 -0400

    hamming: Implement basic strand distance logic
    Count the string differences between the two strand strings A and B.

--- c/hamming/src/hamming.c
+++ c/hamming/src/hamming.c
@@ -1,11 +1,19 @@
 #include "hamming.h"
 
+static int count_char_diffs(char strand_a[], char strand_b[], int strand_len) {
+  int char_diffs = 0;  
+  for ( int i = 0; i < strand_len; i++ ) {
+    if (strand_a[i] != strand_b[i]) char_diffs++;
+  }
+  return char_diffs;
+}
+
 int compute(char strand_a[], char strand_b[]) {
   int result = 0;
   if ( !strand_a || !strand_b ) {
     result = -1; 
   } else {
-    result = strcmp(strand_a, strand_b);
+    result = count_char_diffs(strand_a, strand_b, strlen(strand_a));
   }
   return result;  
 }
