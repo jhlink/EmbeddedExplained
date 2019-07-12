b79ec64049dbea870de368669d58a5fc1548f6db
jhlink <jhlink@users.noreply.github.com>
2019-07-12 14:54:31 -0400

    hamming: Resolve compilation errors

---
+++ c/hamming/src/hamming.h
@@ -0,0 +1,11 @@
+#ifndef HAMMMING_H
+#define HAMMMING_H 
+
+#include <string.h>
+
+//  Input: Two strings of variable length 
+//  Output:  Returns the # of char differences or 
+//    -1 on error
+int compute(char strand_a[], char strand_b[]);
+
+#endif
