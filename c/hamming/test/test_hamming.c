279ef6a8641c27c5824677e086d6950d6c394a84
jhlink <jhlink@users.noreply.github.com>
2019-07-12 15:14:25 -0400

    hamming: Return error code for unequal strand len

--- c/hamming/test/test_hamming.c
+++ c/hamming/test/test_hamming.c
@@ -26,13 +26,11 @@ static void test_rejects_other_null_strand(void)
 
 static void test_no_difference_between_identical_strands(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(0, compute("A", "A"));
 }
 
 static void test_identical_long_strands(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(0, compute("GGACTGA", "GGACTGA"));
 }
 
@@ -58,13 +56,11 @@ static void test_small_hamming_distance_in_longer_strand(void)
 
 static void test_rejects_extra_length_on_first_strand_when_longer(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(-1, compute("AAAG", "AAA"));
 }
 
 static void test_rejects_extra_length_on_other_strand_when_longer(void)
 {
-   TEST_IGNORE();
    TEST_ASSERT_EQUAL(-1, compute("AAA", "AAAG"));
 }
 
