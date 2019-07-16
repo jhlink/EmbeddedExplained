40793505e65acf402d56b2575ba183f744791524
jhlink <jhlink@users.noreply.github.com>
2019-07-16 11:50:20 -0400

    feat: Implement G->C nucleotide transcription
    The exercise begins with compilation errors.
    
    Necessary files were generated and populated for error-free
    compilation, while implementing the first nucleotide feature.

---
+++ c/rna-transcription/src/rna_transcription.c
@@ -0,0 +1,19 @@
+#include "rna_transcription.h"
+
+static void replace_nucleotide(char* target) {
+  if ( *target == 'G' ) {
+    *target = 'C';
+  }
+}
+
+char* to_rna(const char* dna) { 
+  size_t strand_len = strlen(dna);
+  char* result = (char*) malloc(sizeof(char) * strand_len + 1);
+  strcpy(result, dna);
+
+  for (size_t i = 0; i < strand_len; i++) {
+    replace_nucleotide(result + i); 
+  }
+
+  return result;
+}
---
+++ c/rna-transcription/src/rna_transcription.h
@@ -0,0 +1,9 @@
+#ifndef RNA_H
+#define RNA_H
+
+#include <stdlib.h>
+#include <string.h>
+
+char* to_rna(const char* dna);
+
+#endif
