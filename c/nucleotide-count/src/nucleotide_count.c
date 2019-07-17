6449eb32b5eaf71afa801c974705c7160017d950
jhlink <jhlink@users.noreply.github.com>
2019-07-17 14:50:28 -0400

    fix: Create src/ files to resolve compile errors

---
+++ c/nucleotide-count/src/nucleotide_count.c
@@ -0,0 +1,6 @@
+#include "nucleotide_count.h"
+
+char* count(const char dna_strand[]) {
+  strlen(dna_strand);
+  return NULL;
+}
---
+++ c/nucleotide-count/src/nucleotide_count.h
@@ -0,0 +1,8 @@
+#ifndef NUCLEO_COUNT_H
+#define NUCLEO_COUNT_H
+
+#include <string.h>
+
+char* count(const char dna_strand[]);
+
+#endif
