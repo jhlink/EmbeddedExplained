44aaf8ce1051a682c345c23f65369e19ece8ffa3
jhlink <jhlink@users.noreply.github.com>
2019-07-17 15:40:41 -0400

    feat: Implement string template given empty str

--- c/nucleotide-count/src/nucleotide_count.c
+++ c/nucleotide-count/src/nucleotide_count.c
@@ -1,6 +1,21 @@
 #include "nucleotide_count.h"
 
-char* count(const char dna_strand[]) {
-  strlen(dna_strand);
-  return NULL;
+char* count(const char dna_strand[]) 
+{
+  size_t strand_len = strlen(dna_strand);
+  nucleo_count* nucleo_ct = (nucleo_count*) calloc(1, sizeof(nucleo_count)); 
+
+  if (strand_len > 0) {
+
+  }
+
+  int sz = snprintf(NULL, 0, NUCLEO_COUNT_STRING_TEMPLATE, nucleo_ct->A, nucleo_ct->C, nucleo_ct->G, nucleo_ct->T );   
+
+  char* nucleo_ct_result = (char*) malloc(sz + 1); 
+
+  snprintf(nucleo_ct_result, sz + 1, NUCLEO_COUNT_STRING_TEMPLATE, nucleo_ct->A, nucleo_ct->C, nucleo_ct->G, nucleo_ct->T );   
+
+  free(nucleo_ct);
+
+  return nucleo_ct_result;
 }
--- c/nucleotide-count/src/nucleotide_count.h
+++ c/nucleotide-count/src/nucleotide_count.h
@@ -2,6 +2,18 @@
 #define NUCLEO_COUNT_H
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUCLEO_COUNT_STRING_TEMPLATE "A:%i C:%i G:%i T:%i"
+
+typedef struct NUCLEO_COUNT {
+  int A;
+  int C;
+  int G;
+  int T;
+} nucleo_count;
+
 
 char* count(const char dna_strand[]);
 
