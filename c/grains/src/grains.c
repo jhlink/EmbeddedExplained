80e3677a939b5753c7fed84b2027f7e9d17a7420
jhlink <jhlink@users.noreply.github.com>
2019-07-19 13:37:25 -0400

    refactor: Improve readability of ull with typedef

--- c/grains/src/grains.c
+++ c/grains/src/grains.c
@@ -1,13 +1,13 @@
 #include "grains.h"
 
-unsigned long long square(const int square_index) {
+ULL_INT square(const int square_index) {
   bool is_invalid = !square_index || square_index > 64;
-  unsigned long long result = is_invalid ? 0ull : 1ull << (square_index - 1); 
+  ULL_INT result = is_invalid ? 0ull : 1ull << (square_index - 1); 
   return result;
 }
 
-unsigned long long total() {
-  unsigned long long sum = 0ull;
+ULL_INT total() {
+  ULL_INT sum = 0ull;
   for (int i = 0; i < 65; i++) {
     sum += square(i); 
   }
--- c/grains/src/grains.h
+++ c/grains/src/grains.h
@@ -2,9 +2,10 @@
 #define GRAINS_H
 
 #include <stdbool.h>
-#include <stdio.h>
 
-unsigned long long square(const int square_index);
-unsigned long long total();
+typedef unsigned long long ULL_INT;
+
+ULL_INT square(const int square_index);
+ULL_INT total();
 
 #endif
