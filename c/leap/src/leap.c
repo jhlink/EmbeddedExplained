29a3dde33d08d17885477cbaa2911d484f887b97
jhlink <jhlink@users.noreply.github.com>
2019-07-30 17:30:24 -0400

    refactor: Rename variables for clarity

--- c/leap/src/leap.c
+++ c/leap/src/leap.c
@@ -1,9 +1,9 @@
 #include "leap.h"
 
 bool is_leap_year(const int year) {
-  bool divisible_by_4 = year % 4 == 0;
-  bool ignore_centuries = year % 100 == 0;
-  bool divisible_by_400 = year % 400 == 0; 
+  bool is_divisible_by_4 = year % 4 == 0;
+  bool is_century = year % 100 == 0;
+  bool is_divisible_by_400 = year % 400 == 0; 
 
-  return ignore_centuries ? divisible_by_400 : divisible_by_4;
+  return is_century ? is_divisible_by_400 : is_divisible_by_4;
 }
