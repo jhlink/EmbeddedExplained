099a7d8c7a2291498e941acd8b4d4c1cdb9987f9
jhlink <jhlink@users.noreply.github.com>
2019-07-30 17:28:19 -0400

    feat: Identify leap years every 400th year

--- c/leap/src/leap.c
+++ c/leap/src/leap.c
@@ -1,7 +1,9 @@
 #include "leap.h"
 
 bool is_leap_year(const int year) {
-  bool divisible_by_four = year % 4 == 0;
+  bool divisible_by_4 = year % 4 == 0;
   bool ignore_centuries = year % 100 == 0;
-  return ignore_centuries ? false : divisible_by_four;
+  bool divisible_by_400 = year % 400 == 0; 
+
+  return ignore_centuries ? divisible_by_400 : divisible_by_4;
 }
