b88ffb0b9d975b31afe06dd8ed5ab1252a212e3b
jhlink <jhlink@users.noreply.github.com>
2019-07-30 17:20:28 -0400

    feat: Ignore leap years every century

--- c/leap/src/leap.c
+++ c/leap/src/leap.c
@@ -2,5 +2,6 @@
 
 bool is_leap_year(const int year) {
   bool divisible_by_four = year % 4 == 0;
-  return divisible_by_four;
+  bool ignore_centuries = year % 100 == 0;
+  return ignore_centuries ? false : divisible_by_four;
 }
