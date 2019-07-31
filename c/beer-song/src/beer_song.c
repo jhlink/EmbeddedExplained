77d498c14d59b86fea4ae37099eef3c7463dd5ce
jhlink <jhlink@users.noreply.github.com>
2019-07-31 18:19:09 -0400

    feat: Implement plural bottles for beer song

--- c/beer-song/src/beer_song.c
+++ c/beer-song/src/beer_song.c
@@ -1,7 +1,8 @@
 #include "beer_song.h"
 
 void verse(char* char_buf, int verse_count) {
-  printf(char_buf, "%d", verse_count);
+  int verse_start_end_index = sprintf(char_buf, VERSE_START_TEMPLATE, verse_count, verse_count);
+  sprintf(char_buf + verse_start_end_index, VERSE_END_TEMPLATE, verse_count - 1);
 }
 
 void sing(char* char_buf, int start_verse, int end_verse) {
--- c/beer-song/src/beer_song.h
+++ c/beer-song/src/beer_song.h
@@ -4,6 +4,11 @@
 #include <string.h>
 #include <stdio.h>
 
+#define DEFAULT_BUFFER_SIZE 1024
+
+static const char* VERSE_START_TEMPLATE = "%d bottles of beer on the wall, %d bottles of beer.\n";
+static const char* VERSE_END_TEMPLATE = "Take one down and pass it around, %d bottles of beer on the wall.\n";
+
 void verse(char* char_buf, int verse_count); 
 void sing(char* char_buf, int start_verse, int end_verse);
 
