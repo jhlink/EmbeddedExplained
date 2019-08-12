02d2c88a27de96d63495015d19505e8577d0d584
jhlink <jhlink@users.noreply.github.com>
2019-08-12 17:06:10 -0400

    refactor: Encapsulation verse creation
    Creating a verse is filled with too many conditionals and mixed
    functionality.
    
    Use a struct as a container for variant verses and encapsulate verse
    creation and buffer population.

--- c/beer-song/src/beer_song.c
+++ c/beer-song/src/beer_song.c
@@ -1,22 +1,42 @@
 #include "beer_song.h"
 
-void verse(char* char_buf, int verse_count) {
-  int verse_start_end_index = 0;
-  if ( verse_count == 0 ) {
-    verse_start_end_index = sprintf(char_buf, "%s", VERSE_START_FINAL_TEMPLATE);
+static void construct_verse(VerseTemplate *vt, int verse_count) {
+  if ( verse_count == 2 ) {
+    vt->start_template = &VERSE_START_PLURAL_TEMPLATE;
+    vt->end_template = &VERSE_END_SINGLE_TEMPLATE;
+  } else if ( verse_count == 1 ) {
+    vt->start_template = &VERSE_START_SINGLE_TEMPLATE;
+    vt->end_template = &VERSE_END_NONE_TEMPLATE;
+  } else if ( verse_count == 0 ) {
+    vt->start_template = &VERSE_START_FINAL_TEMPLATE;
+    vt->end_template = &VERSE_END_FINAL_TEMPLATE;
   } else {
-    verse_start_end_index = sprintf(char_buf, verse_count > 1 ? VERSE_START_PLURAL_TEMPLATE : VERSE_START_SINGLE_TEMPLATE, verse_count, verse_count);
+    vt->start_template = &VERSE_START_PLURAL_TEMPLATE;
+    vt->end_template = &VERSE_END_PLURAL_TEMPLATE;
+  }
+}
+
+static void fill_buffer(char* char_buf, VerseTemplate* vt, int verse_count) {
+  int verse_start_end_index = 0;
+
+  if ( verse_count < 2 ) {
+    verse_start_end_index = sprintf(char_buf, "%s", *(vt->start_template));
+  } else {
+    verse_start_end_index = sprintf(char_buf, *(vt->start_template), verse_count, verse_count);
   }
 
-  if ( verse_count == 2 ) {
-    sprintf(char_buf + verse_start_end_index, "%s", VERSE_END_SINGLE_TEMPLATE);
-  } else if ( verse_count == 1 ) {
-    sprintf(char_buf + verse_start_end_index, "%s", VERSE_END_NONE_TEMPLATE);
-  } else if ( verse_count == 0 ) {
-    sprintf(char_buf + verse_start_end_index, "%s", VERSE_END_FINAL_TEMPLATE);
+  if ( verse_count < 3 ) {
+    sprintf(char_buf + verse_start_end_index, "%s", *(vt->end_template));
   } else {
-    sprintf(char_buf + verse_start_end_index, VERSE_END_PLURAL_TEMPLATE, verse_count - 1);
+    sprintf(char_buf + verse_start_end_index, *(vt->end_template), verse_count - 1);
   }
+}
+
+void verse(char* char_buf, int verse_count) {
+  VerseTemplate* vt = (VerseTemplate*) malloc(sizeof( VerseTemplate ));
+
+  construct_verse(vt, verse_count);
+  fill_buffer(char_buf, vt, verse_count);
 }
 
 void sing(char* char_buf, int start_verse, int end_verse) {
