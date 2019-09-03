1ab234c37b3cb4bd051e787b37236c5051745a02
jhlink <jhlink@users.noreply.github.com>
2019-09-03 17:32:52 -0400

    feat: Implement multiply feature

--- comp_arch/nand2tetris/projects/04/mult/mult.asm
+++ comp_arch/nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+  //  Initialize variables
+  @n
+  M=0
+  @R2
+  M=0
+
+(LOOP)
+  @R1
+  D=M
+  @n
+  D=D-M
+  @STOP
+  D; JEQ
+  @R0
+  D=M
+  @R2
+  M=D+M
+  @n
+  M=M+1
+  @LOOP
+  0; JMP
+
+(STOP)
+  @STOP
+  0; JMP
+
